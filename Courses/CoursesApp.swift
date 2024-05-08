23e1d9182baefae32775fddbc99d6c547a8ef7d6
Kakimbek Nyssanov <kakimbekn@gmail.com>
2024-05-08 16:48:45 +0500

    Initial Commit

---
+++ Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by kakim nyssanov on 08.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
